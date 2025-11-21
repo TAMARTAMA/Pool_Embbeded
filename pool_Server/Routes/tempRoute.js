7b69d5a72ef100e5a5be92b08bc7700dbb74f9bd
Tamar Heksher <ts0556726468@gmail.com>
2025-11-21 11:56:27 +0200

    Update tempRoute.js

--- pool_Server/Routes/tempRoute.js
+++ pool_Server/Routes/tempRoute.js
@@ -1,6 +1,7 @@
-const router=require('express').Router()//יבוא של ראוטר של אקספרס הוא אחראי לנתב לפונקציה המתאימה
+const router=require('express').Router()
 const tController=require('../Controllers/temperatureController')
 router.get('/getTemp', tController.getCurrentWaterTemperature)
 router.get('/getHistory', tController.getUsageStats)
 router.post('/addTemp', tController.addTemperatureMeasurement)
-module.exports=router// ייצוא כדי שנוכל להפעיל מהAPP
+
+module.exports=router
