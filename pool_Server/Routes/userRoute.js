09d0834c0b208a4a0105bf23e69a74f2426653c9
Tamar Heksher <ts0556726468@gmail.com>
2025-11-21 11:56:49 +0200

    Update userRoute.js

--- pool_Server/Routes/userRoute.js
+++ pool_Server/Routes/userRoute.js
@@ -1,8 +1,9 @@
-const router=require('express').Router()//יבוא של ראוטר של אקספרס הוא אחראי לנתב לפונקציה המתאימה
+const router=require('express').Router()
 const userController=require('../Controllers/userController')
 router.delete('/removeUser:userId', userController.removeUser)
 router.post('/register', userController.register)
 router.get('/getAll', userController.getAllusers)
 router.get('/login', userController.login)
 router.get('/getRecent', userController.getLastUserEntry)
-module.exports=router// ייצוא כדי שנוכל להפעיל מהAPP
+
+module.exports=router
