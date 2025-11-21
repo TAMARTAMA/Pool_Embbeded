92b15bb832d44c436d584b28af87884d6487991c
Tamar Heksher <ts0556726468@gmail.com>
2025-11-21 12:06:07 +0200

    Update RFID.ino

--- pool_ardouino/RFID.ino
+++ pool_ardouino/RFID.ino
@@ -1,24 +1,23 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN 5    // ניתן לשנות לפי חיבור
+#define SS_PIN 5    
 #define RST_PIN 4 
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 
 String knownTags[] = {
-  "A1 B2 C3 D4", // דוגמאות לצ'יפים שמוכרים מראש
+  "A1 B2 C3 D4", 
   "11 22 33 44"
   
 };
 
 void InitRFID() {
   SPI.begin();
-  rfid.PCD_Init(); // אתחול החיישן
+  rfid.PCD_Init(); 
   Serial.println("RFID system ready");
 }
 
-// מדליקה את החיישן ומחזירה UID שנקרא או מחרוזת ריקה
 String activateRFID() {
   if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
     return "NO";
@@ -36,13 +35,11 @@ String activateRFID() {
   return tagID;
 }
 
-// מכבה את החיישן
 void deactivateRFID() {
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
 }
 
-// בודקת האם ה־tagID נמצא ברשימת תגיות מוכרות
 bool isKnownTag(String tag) {
   for (String known : knownTags) {
     if (known == tag) {
@@ -51,3 +48,4 @@ bool isKnownTag(String tag) {
   }
   return false;
 }
+
