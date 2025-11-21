6413a07410d0d8ae69cf5a0cb5cb18976a78f304
Tamar Heksher <ts0556726468@gmail.com>
2025-11-21 11:55:42 +0200

    Update emergencyRoute.js

--- pool_Server/Routes/emergencyRoute.js
+++ pool_Server/Routes/emergencyRoute.js
@@ -1,6 +1,7 @@
-const router=require('express').Router()//יבוא של ראוטר של אקספרס הוא אחראי לנתב לפונקציה המתאימה
+const router=require('express').Router()
 const emController=require('../Controllers/emergencyController')
 router.get('/getHistory:userId', emController.getEmergencyHistory)
 router.get('/getHistory', emController.getEmergencyHistory)
 router.post('/add', emController.addEmergencyAlert)
-module.exports=router// ייצוא כדי שנוכל להפעיל מהAPP
+
+module.exports=router
